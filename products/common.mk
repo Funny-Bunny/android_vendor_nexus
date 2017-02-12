e64cbe7329a858de182e9019622e6ac1c2cbd73c
Funny-Bunny <dragos281993@gmail.com>
2017-02-12 19:43:00 +0200

    base

---
+++ products/common.mk
@@ -0,0 +1,32 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    ro.com.android.dataroaming=false \
+    drm.service.enabled=true \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.build.selinux=1 \
+    ro.adb.secure=0
+
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/nexus/overlay/common
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Latin IME lib - gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    Busybox \
+    LockClock
