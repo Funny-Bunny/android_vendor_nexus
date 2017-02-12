4f46fb345bd4a6fb6675c08d32fae11b9ca28c13
Funny-Bunny <dragos281993@gmail.com>
2017-02-12 19:53:28 +0200

    Enable Storage manager

--- products/common.mk
+++ products/common.mk
@@ -14,7 +14,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
     ro.build.selinux=1 \
-    ro.adb.secure=0
+    ro.adb.secure=0 \
+    ro.storage_manager.enabled=1
 
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += \
